a5824417c77cbd934ba0d82bc92997e5002d1803
chprab <ch.prab@gmail.com>
2018-01-10 23:46:07 +0530

    Updated comment

--- src/main/java/com/tut/git/CalculatorTest.java
+++ src/main/java/com/tut/git/CalculatorTest.java
@@ -3,6 +3,9 @@ package com.tut.git;
 public class CalculatorTest {
 
 	public static void main(String[] args) {
+		
+		//Test Comment
+		
 		ICalculator calculator = new CalculatorImpl();
 		
 		System.out.println(calculator.add(12, 15));
